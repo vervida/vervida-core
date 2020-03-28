b24946a36b5b303cb928b388b8c046d8c04312e0
Srevin Saju <srevin03@gmail.com>
2020-03-28 18:56:35 +0300

    Add Google Auth

--- assets/js/vervida-firebase.js
+++ assets/js/vervida-firebase.js
@@ -50,7 +50,7 @@ function VervidaSignOut(){
     })
     .catch(function(error) {
       // TODO FIXME
-      alert(`Couldn't sign you out. Some Error occureed `)
+      alert(`Couldn't sign you out. Some Error occurred `)
     });
 }
 
@@ -62,5 +62,30 @@ function VervidaRedirectNotLoggedIn(){
     } else {
       window.location.replace("examples/register.html");
     }
+  });
+}
+
+function VervidaSignInGoogle(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  firebase.auth().signInWithPopup(provider)
+  .then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    window.location.replace("/");
+    // ...
+  })
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    alert(`Couldn't sign you in. Error : ${errorMessage}`)
+    // ...
   });
 }
